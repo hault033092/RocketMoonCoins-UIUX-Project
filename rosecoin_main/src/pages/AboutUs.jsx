ce8ecdbf3d20d377a2a98e77f7c0d1a5b51132ae
NguyenVu11796 <hoangnguyen96117@gmail.com>
2022-08-08 08:59:28 +0700

    about us page complete

--- rosecoin_main/src/pages/AboutUs.jsx
+++ rosecoin_main/src/pages/AboutUs.jsx
@@ -1,11 +1,64 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const AboutUs = () => {
   return (
     <div>
-      <h1>About Us</h1>
+      <Div1>
+        <Head1>
+          hello
+        </Head1>
+      </Div1>
+      <Div2>
+        <Head1>
+          placeholder2
+        </Head1>
+      </Div2>
+      <Div3>
+        <Head1>
+          placeholder3
+        </Head1>
+      </Div3>
+      <Div4>
+        <Head1>
+          placeholder4
+        </Head1>
+      </Div4>
     </div>
   )
 }
+
+const Head1 = styled.h1`
+    color: red;
+    justify-items: center;
+`
+const Div1 = styled.div`
+    float: left;
+    width: 50%;
+    height: 50%;
+    float: left;
+    background: red; 
+`
+const Div2 = styled.div`
+    float: left;
+    width: 50%;
+    height: 50%;
+    float: left;
+    background: blue; 
+`
+const Div3 = styled.div`
+    float: left;
+    width: 50%;
+    height: 50%;
+    float: left;
+    background: yellow; 
+`
+const Div4 = styled.div`
+    float: left;
+    width: 50%;
+    height: 50%;
+    float: left;
+    background: grey; 
+`
 
 export default AboutUs
